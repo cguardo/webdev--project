4880b21f34e0da36471f155f61b49ec50c4b094b
Danica Lacra <lacra.danica@ue.edu.ph>
2022-10-24 07:49:15 +0800

    Validation Step 1-3

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -28,89 +28,7 @@ jQuery(document).ready(function() {
     	var parent_fieldset = $(this).parents('fieldset');
     	var next_step = true;
 
-		parent_fieldset.find('input[type="email"]').each(function() {
-    		if($(this).val() == "" ) {
-    			$(this).addClass('input-error');
-				alert("Please input on required slots")
-    			next_step = false;	
-    		}
-			else if ($(this).val()) {
-				var emailregex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-				var email = $(this).val();
-				if (emailregex.test(email)){
-					next_step = true;	
-				}
-				else{
-					$(this).addClass('input-error');
-					alert('Invalid email');
-					next_step = false;
-				}
-			}
-
-    		else {
-    			$(this).removeClass('input-error');
-    		}
-    	});
-
-		parent_fieldset.find('input[type="ueemail"]').each(function() {
-    		if($(this).val() == "" ) {
-    			$(this).addClass('input-error');
-				alert("Please input on required slots")
-    			next_step = false;	
-    		}
-			else if ($(this).val()) {
-				var ueemailregex = /^([a-zA-Z0-9_.+-])+\@(ue.edu.ph)$/;
-				var ueemail = $(this).val();
-				if (ueemailregex.test(ueemail)){
-					
-				}
-				else{
-					$(this).addClass('input-error');
-					alert('Invalid email');
-					next_step = false;
-				}
-			}
-
-    		else {
-    			$(this).removeClass('input-error');
-    		}
-    	});
-
-
-		parent_fieldset.find('input[type="studnum"]').each(function() {
-    		if($(this).val() == "" ) {
-    			$(this).addClass('input-error');
-				alert("Please input on required slots")
-    			next_step = false;	
-    		}
-			else if ($(this).val()) {
-				var filter = /^(20)\d{9}/;
-				var studNum = $(this).val();
-				if (filter.test(studNum)){
-					if(studNum.length==11){
-						next_step = true;
-					}
-					else{
-						$(this).addClass('input-error');
-						alert('Not a valid number');
-						next_step = false;
-					}
-					
-				}
-				else{
-					$(this).addClass('input-error');
-					alert('Invalid input');
-					next_step = false;
-				}
-			}
-
-    		else {
-    			$(this).removeClass('input-error');
-    		}
-
-    	});
-
-		parent_fieldset.find('select').each(function() {
+/*		parent_fieldset.find('select').each(function() {
     		if( $(this).val() == "" ) {
     			$(this).addClass('input-error');
 				alert("Please choose any of the given")
@@ -133,6 +51,262 @@ jQuery(document).ready(function() {
     			$(this).removeClass('input-error');
     		}
     	});
+*/
+		parent_fieldset.find('input[id="form-first-name"]').each(function() {
+    		if( $(this).val() == "" ) {
+    			$(this).addClass('input-error');
+				alert("First Name cannot be blank.")
+    			next_step = false;
+				
+    		}
+		});
+
+		parent_fieldset.find('input[id="form-last-name"]').each(function() {
+    		if( $(this).val() == "" ) {
+    			$(this).addClass('input-error');
+				alert("Last Name cannot be blank.")
+    			next_step = false;
+				
+    		}
+		});
+
+		parent_fieldset.find('input[type="studnum"]').each(function() {
+    		if($(this).val() == "" ) {
+    			$(this).addClass('input-error');
+				alert("Student Number cannot be blank.")
+    			next_step = false;	
+    		}
+			else if ($(this).val()) {
+				var filter = /^(20)\d{9}/;
+				var studNum = $(this).val();
+				if (filter.test(studNum)){
+					if(studNum.length==11){
+						next_step = true;
+					}
+					else{
+						$(this).addClass('input-error');
+						alert('Please enter an 11-digit student number.');
+						next_step = false;
+					}
+					
+				}
+				else{
+					$(this).addClass('input-error');
+					alert('Please enter an 11-digit student number.');
+					next_step = false;
+				}
+			}
+
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+
+    	});
+
+		parent_fieldset.find('select[id="form-year-level"]').each(function() {
+    		if( $(this).val() == "" ) {
+    			$(this).addClass('input-error');
+				alert("Please choose a Year Level.")
+    			next_step = false;
+				
+    		}
+		});
+
+		parent_fieldset.find('select[id="form-program"]').each(function() {
+    		if( $(this).val() == "" ) {
+    			$(this).addClass('input-error');
+				alert("Please choose a Program.")
+    			next_step = false;
+				
+    		}
+		});
+
+	/*
+        Step 2/5
+    */
+	
+		parent_fieldset.find('input[id="form-ue-email"]').each(function() {
+    		if($(this).val() == "" ) {
+    			$(this).addClass('input-error');
+				alert("UE Email Address cannot be blank.")
+    			next_step = false;	
+    		}
+			else if ($(this).val()) {
+				var ueemailregex = /^([a-zA-Z0-9_.+-])+\@(ue.edu.ph)$/;
+				var ueemail = $(this).val();
+				if (ueemailregex.test(ueemail)){
+					
+				}
+				else{
+					$(this).addClass('input-error');
+					alert('The inputted UE Email Address is invalid. Please enter a valid school Email Address.');
+					next_step = false;
+				}
+			}
+
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+
+
+		parent_fieldset.find('input[id="form-personal-email"]').each(function() {
+    		if($(this).val() == "" ) {
+    			$(this).addClass('input-error');
+				alert("Personal Email Address cannot be blank.")
+    			next_step = false;	
+    		}
+			else if ($(this).val()) {
+				var emailregex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+				var email = $(this).val();
+				if (emailregex.test(email)){
+					next_step = true;	
+				}
+				else{
+					$(this).addClass('input-error');
+					alert('The inputted Personal Email Address is invalid. Please enter a valid Email Address.');
+					next_step = false;
+				}
+			}
+
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+		
+
+		parent_fieldset.find('input[id="form-contact-number"]').each(function() {
+			if($(this).val() == "" ) {
+			   $(this).addClass('input-error');
+				alert("Contact Number cannot be blank.")
+			   next_step = false;	
+		   }
+			else if ($(this).val()) {
+				var phoneregex = /^(09)\d{9}$/;
+				var mobNum = $(this).val();
+				if (phoneregex.test(mobNum)){
+					if(mobNum.length==11){
+						next_step = true;
+					}
+					else{
+						$(this).addClass('input-error');
+					   alert('The inputted Contact Number is invalid. Please enter an 11-digit contact number.');
+						next_step = false;
+					}
+				   
+				}
+				else{
+					$(this).addClass('input-error');
+					alert('The inputted Contact Number is invalid. Please enter an 11-digit contact number.');
+					next_step = false;
+				}
+			}
+
+			else {
+				$(this).removeClass('input-error');
+			}
+	   });
+
+	   	parent_fieldset.find('input[id="form_home_address"]').each(function() {
+    		if( $(this).val() == "" ) {
+    			$(this).addClass('input-error');
+				alert("Home Address cannot be blank.")
+    			next_step = false;
+				
+    		}
+		});
+
+		parent_fieldset.find('input[id="form_provincial_address"]').each(function() {
+    		if( $(this).val() == "" ) {
+    			$(this).addClass('input-error');
+				alert("Provincial Address cannot be blank.")
+    			next_step = false;
+				
+    		}
+		});
+
+		/* step 3/5 */
+
+		parent_fieldset.find('input[id="form-fullname-emergency"]').each(function() {
+    		if( $(this).val() == "" ) {
+    			$(this).addClass('input-error');
+				alert("The Name cannot be blank.")
+    			next_step = false;
+				
+    		}
+		});
+		
+		parent_fieldset.find('input[id="form-relationship"]').each(function() {
+    		if( $(this).val() == "" ) {
+    			$(this).addClass('input-error');
+				alert("The Relationship cannot be blank.")
+    			next_step = false;
+				
+    		}
+		});
+
+		parent_fieldset.find('input[id="form-contact-numberEmergency"]').each(function() {
+			if($(this).val() == "" ) {
+			   $(this).addClass('input-error');
+				alert("Contact Number cannot be blank.")
+			   next_step = false;	
+		   }
+			else if ($(this).val()) {
+				var phoneregex = /^(09)\d{9}$/;
+				var mobNum = $(this).val();
+				if (phoneregex.test(mobNum)){
+					if(mobNum.length==11){
+						next_step = true;
+					}
+					else{
+						$(this).addClass('input-error');
+					   alert('The inputted Contact Number is invalid. Please enter an 11-digit contact number.');
+						next_step = false;
+					}
+				   
+				}
+				else{
+					$(this).addClass('input-error');
+					alert('The inputted Contact Number is invalid. Please enter an 11-digit contact number.');
+					next_step = false;
+				}
+			}
+
+			else {
+				$(this).removeClass('input-error');
+			}
+	   });
+
+	   	parent_fieldset.find('input[id="form-home-addressEmergency"]').each(function() {
+			if( $(this).val() == "" ) {
+				$(this).addClass('input-error');
+				alert("Home Address cannot be blank.")
+				next_step = false;
+			
+			}
+		});
+
+		parent_fieldset.find('input[id="form-facebook-username"]').each(function() {
+			if ($(this).val()) {
+				var fbusernameex = /^([a-zA-Z0-9_.+-])$/;
+				var fbusername = $(this).val();
+				if (fbusernameex.test(fbusername)){
+					next_step = true;
+				}
+				else{
+					$(this).addClass('input-error');
+					alert('The inputted Facebook Username is invalid. Please enter a valid username.');
+					next_step = false;
+				}
+			}
+
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+
+
+		
 
 
 		if( next_step ) {
@@ -146,37 +320,6 @@ jQuery(document).ready(function() {
 
 
 
-		// parent_fieldset.find('input[type="contact"]').each(function() {
-    	// 	if($(this).val() == "" ) {
-    	// 		$(this).addClass('input-error');
-		// 		alert("Please input on required slots")
-    	// 		next_step = false;	
-    	// 	}
-		// 	else if ($(this).val()) {
-		// 		var phoneregex = /^(09)\d{9}$/;
-		// 		var mobNum = $(this).val();
-		// 		if (phoneregex.test(mobNum)){
-		// 			if(mobNum.length==11){
-		// 				next_step = true;
-		// 			}
-		// 			else{
-		// 				$(this).addClass('input-error');
-		// 				alert('Not a valid phone number');
-		// 				next_step = false;
-		// 			}
-					
-		// 		}
-		// 		else{
-		// 			$(this).addClass('input-error');
-		// 			alert('Invalid input');
-		// 			next_step = false;
-		// 		}
-		// 	}
-
-    	// 	else {
-    	// 		$(this).removeClass('input-error');
-    	// 	}
-    	// });
 
     	
     // 	if( next_step ) {
